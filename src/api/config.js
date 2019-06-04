8cfc618f379e4eeb28471479b44ee9d7b891a801
Mario Salinas <mario.salinas.arenas@gmail.com>
2019-06-03 21:59:01 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '8f3a0faf81cdbb8fee6a171cfbfb0b44',
+    secret: '0bca3a9989ffe786d67ba7eb7bd4929c',
+    registeredTo: 'msalinas87'
+}
+
+// Application name	platzimusic
+// API key	8f3a0faf81cdbb8fee6a171cfbfb0b44
+// Shared secret	0bca3a9989ffe786d67ba7eb7bd4929c
+// Registered to	msalinas87
